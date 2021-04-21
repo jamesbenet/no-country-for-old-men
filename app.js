8ee483f631aee2be5c56b0baf807a0499d6e5a00
jbenet <nycflasher@yahoo.com>
2021-04-21 17:52:34 -0400

    scrapped old dropdown and recoded

--- app.js
+++ app.js
@@ -82,71 +82,71 @@ console.log(selectsCanada)
 
 // https://www.freecodecamp.org/news/how-to-create-a-dropdown-menu-with-css-and-javascript/
 //CLICKING ON DROPDOWN ELEMENT
-function toggleClass(elem,className){
-	if (elem.className.indexOf(className) !== -1){
-		elem.className = elem.className.replace(className,'');
-	}
-	else{
-		elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
-	}
+// function toggleClass(elem,className){
+// 	if (elem.className.indexOf(className) !== -1){
+// 		elem.className = elem.className.replace(className,'');
+// 	}
+// 	else{
+// 		elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
+// 	}
 	
-	return elem;
-}
+// 	return elem;
+// }
 
-function toggleDisplay(elem){
-	const curDisplayStyle = elem.style.display;			
+// function toggleDisplay(elem){
+// 	const curDisplayStyle = elem.style.display;			
 				
-	if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-		elem.style.display = 'block';
-	}
-	else{
-		elem.style.display = 'none';
-	}
-}
+// 	if (curDisplayStyle === 'none' || curDisplayStyle === ''){
+// 		elem.style.display = 'block';
+// 	}
+// 	else{
+// 		elem.style.display = 'none';
+// 	}
+// }
 
 
-function toggleMenuDisplay(e){
-	const dropdown = e.currentTarget.parentNode;
-	const menu = dropdown.querySelector('.menu');
-	const icon = dropdown.querySelector('.fa-angle-right');
+// function toggleMenuDisplay(e){
+// 	const dropdown = e.currentTarget.parentNode;
+// 	const menu = dropdown.querySelector('.menu');
+// 	const icon = dropdown.querySelector('.fa-angle-right');
 
-	toggleClass(menu,'hide');
-	toggleClass(icon,'rotate-90');
-}
-//SELECTING DROPDOWN OPTIONS
-function handleOptionSelected(e){
-	toggleClass(e.target.parentNode, 'hide');			
+// 	toggleClass(menu,'hide');
+// 	toggleClass(icon,'rotate-90');
+// }
+// //SELECTING DROPDOWN OPTIONS
+// function handleOptionSelected(e){
+// 	toggleClass(e.target.parentNode, 'hide');			
 
-	const id = e.target.id;
-	const newValue = e.target.textContent + ' ';
-	const titleElem = document.querySelector('.dropdown .title');
-	const icon = document.querySelector('.dropdown .title .fa');
+// 	const id = e.target.id;
+// 	const newValue = e.target.textContent + ' ';
+// 	const titleElem = document.querySelector('.dropdown .title');
+// 	const icon = document.querySelector('.dropdown .title .fa');
 
 
-	titleElem.textContent = newValue;
-	titleElem.appendChild(icon);
+// 	titleElem.textContent = newValue;
+// 	titleElem.appendChild(icon);
 	
-	//trigger custom event
-	document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
-	//setTimeout is used so transition is properly shown
-	setTimeout(() => toggleClass(icon,'rotate-90',0));
-}
-//CHANGING THE CURRENTLY SELECTED OPTION
-function handleTitleChange(e){
-	const result = document.getElementById('result');
+// 	//trigger custom event
+// 	document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
+// 	//setTimeout is used so transition is properly shown
+// 	setTimeout(() => toggleClass(icon,'rotate-90',0));
+// }
+// //CHANGING THE CURRENTLY SELECTED OPTION
+// function handleTitleChange(e){
+// 	const result = document.getElementById('result');
 
-	result.innerHTML = 'The result is: ' + e.target.textContent;
-}
+// 	result.innerHTML = 'The result is: ' + e.target.textContent;
+// }
 
-//MAIN CODE
-//get elements
-const dropdownTitle = document.querySelector('.dropdown .title');
-const dropdownOptions = document.querySelectorAll('.dropdown .option');
+// //MAIN CODE
+// //get elements
+// const dropdownTitle = document.querySelector('.dropdown .title');
+// const dropdownOptions = document.querySelectorAll('.dropdown .option');
 
-//bind listeners to these elements
-dropdownTitle.addEventListener('click', toggleMenuDisplay);
-dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
-document.querySelector('.dropdown .title').addEventListener('change', handleTitleChange);
+// //bind listeners to these elements
+// dropdownTitle.addEventListener('click', toggleMenuDisplay);
+// dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
+// document.querySelector('.dropdown .title').addEventListener('change', handleTitleChange);
 
 
 
