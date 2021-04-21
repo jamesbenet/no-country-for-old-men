0967fdecde639add3f4f359615ae7df3ea55f86b
jbenet <nycflasher@yahoo.com>
2021-04-21 12:06:10 -0400

    working on JS

--- app.js
+++ app.js
@@ -1,4 +1,88 @@
 console.log("hello")
+
+
+//ENDPOINT= TOTAL # OF GUN DEATHS BY LOCATION
+
+      // https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=total_number_of_gun_deaths&location_id=10&format=raw
+
+
+// let id = 10
+
+// async function gunDeathsByCountry(id) {
+//   const url = `https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=total_number_of_gun_deaths&location_id=${10}&format=raw'
+//   `
+  
+//   try {
+//     const results = await axios.get(url)
+//     const info = results.data.drinks
+
+//     info.forEach((info) => {
+//       searchByDrinkId(info.idDrink)
+//     })
+
+//   }
+//   catch (error) { 
+//     console.log(error)
+//   }
+// }
+
+      // Create dynamic dropdown menu from GET all countries (from dogs-code-along)
+      // https://dog.ceo/api/breeds/list/all
+
+const getOptions = async () => {
+  const url = 'https://cors-anywhere.herokuapp.com/https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getlocations&format=raw' 
+  try {
+    const response = await axios.get(url)
+    let countryList = Object.keys(response.data.message)
+    // setOptions(countryList)
+    // return countryList
+    console.log(countryList)
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+getOptions()
+
+
+
+
+
+
+
+      //ajax intro starter code solution--using country list URL
+
+// const getData = () => {
+  // Remove old element's
+  // removePeople()
+
+//   const url = 'https://cors-anywhere.herokuapp.com/https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=total_number_of_gun_deaths&location_id=10&format=raw'
+
+//   axios.get(url)
+//     .then((res) => {
+//       // console.log(res.data.results)
+//       const response = res.data.results
+//       console.log(response)
+//     })
+  
+//   .catch((error) => {
+//     console.error(error)
+//   })
+
+
+// }
+
+// getData()
+
+
+
+
+
+
+
+
+
 // https://www.freecodecamp.org/news/how-to-create-a-dropdown-menu-with-css-and-javascript/
 //CLICKING ON DROPDOWN ELEMENT
 function toggleClass(elem,className){
