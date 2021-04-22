0d56eceefb928c2b30dff66d113bbe9184b5d8a5
jbenet <nycflasher@yahoo.com>
2021-04-22 17:56:59 -0400

    all data returns

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// console.log("hello")
-
 
 
 // https://cors-anywhere.herokuapp.com/
@@ -10,12 +8,22 @@
 
 
 
-// 1. CORS error
-// 2. https://cors-anywhere.herokuapp.com/ (was working, isn't know)
-// 3. https://github.com/jamesbenet/no-country-for-old-men
 
 
 
+//get location value on click submit; the call get data function
+const gun = document.querySelector('button')
+gun.addEventListener('click', () => {
+  // e.preventDefault()
+  // removeCountry()
+  var myobj = document.getElementById("country-data")
+  myobj.remove()
+  const location = document.querySelector('select').value
+  getGunDeaths(location)
+  getGunDeathRate(location)
+  getGunsOwned(location)
+  getGunsOwnedRate(location)
+})
 
 
 const getGunDeaths = async (countryId) => {
@@ -23,11 +31,53 @@ const getGunDeaths = async (countryId) => {
   try {
     const response = await axios.get(url)
     console.log(response)
-    // let gunDeathsList = Object.keys(response.data[0])
-    // console.log(gunDeathsList)
-    // setOptions(countryList)
-    // return countryList
-    // console.log(countryList)
+    let gunDeaths = response.data.result 
+    // display results on page 
+    showDeathData(gunDeaths)
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+const getGunDeathRate = async (countryId) => {
+  const url = `https://cors-anywhere.herokuapp.com/https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=rate_of_all_gun_deaths_per_100_000_people&location_id=${countryId}&format=raw` 
+  try {
+    const response = await axios.get(url)
+    // console.log(response)
+    let gunDeathRate = response.data.result 
+    //display results on page 
+    showDeathRate(gunDeathRate)
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+const getGunsOwned = async (countryId) => {
+  const url = `https://cors-anywhere.herokuapp.com/https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=number_of_privately_owned_firearms&location_id=${countryId}&format=raw` 
+  try {
+    const response = await axios.get(url)
+    console.log(response)
+    let gunsOwned = response.data.result
+    console.log(gunsOwned)
+    //display results on page 
+    showGunsOwned(gunsOwned)
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+const getGunsOwnedRate = async (countryId) => {
+  const url = `https://cors-anywhere.herokuapp.com/https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=rate_of_civilian_firearm_possession&location_id=${countryId}&format=raw` 
+  try {
+    const response = await axios.get(url)
+    console.log(response)
+    let gunsOwnedRate = response.data.result 
+    console.log(gunsOwnedRate)
+    //display results on page 
+    showGunsOwnedRate(gunsOwnedRate)
   }
   catch (error) {
     console.error(error)
@@ -35,184 +85,57 @@ const getGunDeaths = async (countryId) => {
 }
 
 
-function GetSelectedValue(){
-  const e = document.getElementById("country-select");
-  const result = e.options[e.selectedIndex].value;
-  // console.log(result)
-  getGunDeaths(result)
-  // document.getElementById("result").innerHTML = result;
+function showDeathData(data) {
+
+
+  let gunData = `
+  <h1 id="country-name">${data.location}</h1>
+  // <p> Number of total gun deaths, by year: ${data.columnValue} </p>
+  `
+  document.querySelector('#country-data').insertAdjacentHTML('beforeend', gunData)
+  return gunData
+}
+
+function showDeathRate(data) {
+  let gunData = `
+  <p> Rate of All Gun Deaths per 100,000 People: ${data.columnValue} </p>
+  `
+  document.querySelector('#country-data').insertAdjacentHTML('beforeend', gunData)
+  return gunData
+}
+
+function showGunsOwned(data) {
+  let gunData = `<p>The estimated total number of guns (both licit and illicit) held by civilians is: ${data.columnValue}</p>`
+  document.querySelector('#country-data').insertAdjacentHTML('beforeend', gunData)
+  return gunData
+}
+
+function showGunsOwnedRate(data) {
+  let gunData = `<p>The estimated rate of private gun ownership (both licit and illicit) per 100 people is: ${data.columnValue}</p>`
+  document.querySelector('#country-data').insertAdjacentHTML('beforeend', gunData)
+  return gunData
 }
 
 
-// const form1 = document.getElementById('form');
-// const countrySelector = document.getElementById('country-select');
-// const countryId = document.getElementById('country-id');
 
-// console.log(countryId)
+// console.log(gunDeathObject)
+    // console.log(gunDeathObject.location_id)
+    // console.log(gunDeathObject.location)
+    // console.log(gunDeathObject.columnValue)
+    // console.log(gunDeathObject.columnPreamble)
+ // console.log(preamble)
+    // console.log(Object.keys(response))
+    // let gunDeathsList = Object.keys(response.data[0])
+    // console.log(gunDeathsList)
+    // setOptions(countryList)
+    // return countryList
+    // console.log(countryList)
 
-// const countryId = document.getElementById("submit").value
-// let countryID = document.querySelector('#country-select').value;
-// console.log(countryId)
-getGunDeaths()
-
-
-
-
-// function getValue(e) {
-//   e.preventDefault()
-//   const optionValue = document.querySelector('#select-breed').value
-//   console.log(optionValue)
-  // getBreedImage(optionValue)
-// }
-
-
-// Evenhandler for form
-// const form = document.querySelector('form')
-// form.addEventListener("submit", getValue)
-
-
-// const selectsCanada = document.querySelector('.canada')
-// console.log(selectsCanada)
-
-
-// const input = document.querySelector('input')
-// input.addEventListener('click', getGunDeaths)
-
-//ENDPOINT= TOTAL # OF GUN DEATHS BY LOCATION
-
-      // https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=total_number_of_gun_deaths&location_id=10&format=raw
-
-
-      //ajax intro starter code solution--using country list URL
-
-// const getData = () => {
-  // Remove old element's
-  // removePeople()
-
-//   const url = 'https://cors-anywhere.herokuapp.com/https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=total_number_of_gun_deaths&location_id=10&format=raw'
-
-//   axios.get(url)
-//     .then((res) => {
-//       // console.log(res.data.results)
-//       const response = res.data.results
-//       console.log(response)
-//     })
-  
-//   .catch((error) => {
-//     console.error(error)
-//   })
-
-
-// }
-
-// getData()
-
-
-
-
-
-
-
-
-
-// https://www.freecodecamp.org/news/how-to-create-a-dropdown-menu-with-css-and-javascript/
-//CLICKING ON DROPDOWN ELEMENT
-// function toggleClass(elem,className){
-// 	if (elem.className.indexOf(className) !== -1){
-// 		elem.className = elem.className.replace(className,'');
-// 	}
-// 	else{
-// 		elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
-// 	}
-	
-// 	return elem;
-// }
-
-// function toggleDisplay(elem){
-// 	const curDisplayStyle = elem.style.display;			
-				
-// 	if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-// 		elem.style.display = 'block';
-// 	}
-// 	else{
-// 		elem.style.display = 'none';
-// 	}
-// }
-
-
-// function toggleMenuDisplay(e){
-// 	const dropdown = e.currentTarget.parentNode;
-// 	const menu = dropdown.querySelector('.menu');
-// 	const icon = dropdown.querySelector('.fa-angle-right');
-
-// 	toggleClass(menu,'hide');
-// 	toggleClass(icon,'rotate-90');
-// }
-// //SELECTING DROPDOWN OPTIONS
-// function handleOptionSelected(e){
-// 	toggleClass(e.target.parentNode, 'hide');			
-
-// 	const id = e.target.id;
-// 	const newValue = e.target.textContent + ' ';
-// 	const titleElem = document.querySelector('.dropdown .title');
-// 	const icon = document.querySelector('.dropdown .title .fa');
-
-
-// 	titleElem.textContent = newValue;
-// 	titleElem.appendChild(icon);
-	
-// 	//trigger custom event
-// 	document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
-// 	//setTimeout is used so transition is properly shown
-// 	setTimeout(() => toggleClass(icon,'rotate-90',0));
-// }
-// //CHANGING THE CURRENTLY SELECTED OPTION
-// function handleTitleChange(e){
-// 	const result = document.getElementById('result');
-
-// 	result.innerHTML = 'The result is: ' + e.target.textContent;
-// }
-
-// //MAIN CODE
-// //get elements
-// const dropdownTitle = document.querySelector('.dropdown .title');
-// const dropdownOptions = document.querySelectorAll('.dropdown .option');
-
-// //bind listeners to these elements
-// dropdownTitle.addEventListener('click', toggleMenuDisplay);
-// dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
-// document.querySelector('.dropdown .title').addEventListener('change', handleTitleChange);
-
-
-
-
-
-
-
-
-
-
-//ENDPOINT= TOTAL # OF GUN DEATHS BY LOCATION
-
-      // https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=total_number_of_gun_deaths&location_id=10&format=raw
-
-
-// let id = 10
-
-// async function gunDeathsByCountry(id) {
-//   const url = `https://www.gunpolicy.org/index.php?option=com_api&app=gpodatapage&clientid=267&key=69ac9405b94c3b1f632641d48cd80745&resource=getcategorydata&category=total_number_of_gun_deaths&location_id=10&format=raw'
-//   `
-  
-//   try {
-//     const results = await axios.get(url)
-//     const info = results.data.drinks
-
-//     info.forEach((info) => {
-//       searchByDrinkId(info.idDrink)
-//     })
-
-//   }
-//   catch (error) { 
-//     console.log(error)
-//   }
-// }
+    // function removeCountry() {
+    //   // write code here
+    //   // Make sure to call this function???
+    //    const removeElememt = document.querySelector('#country-data')
+    //    while (removeElememt.lastChild) {
+    //      removeElememt.removeChild(removeElememt.lastChild)
+    //    }
+    //  }
